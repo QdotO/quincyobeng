2ef3c921f32f575b5d6dcfa9908e4d2ad9466ed8
Quincy Obeng <quincyobeng@gmail.com>
2022-12-17 18:40:11 -0600

    chore: code cleanup

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Header from '../components/Header'
 import Banner from '../components/Banner/Banner'
 
 export default function Home() {
