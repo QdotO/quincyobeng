9f0277a9ece9c7ad9021a2a7a6222a4f811a4557
Quincy Obeng <quincyobeng@gmail.com>
2023-01-16 13:58:39 -0600

    chore: tailwind possCss integration and bugfix, added sample blog posts

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,26 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import Banner from '../components/Banner/Banner'
+import Banner from '../modules/PersonalSite/components/Banner/Banner'
 
 export default function Home() {
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>QuincyObeng.com</title>
-                <meta name='description' content='Quincy Obeng Personal Site' />
-            </Head>
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>QuincyObeng.com</title>
+        <meta name='description' content='Quincy Obeng Personal Site' />
+      </Head>
 
-            <main className={styles.main}>
-                <Banner />
-            </main>
+      <main className={styles.main}>
+        <Banner />
+      </main>
 
-            <footer className={styles.footer}>Copyright 2022</footer>
-        </div>
-    )
+      <footer className={styles.footer}>Copyright 2022</footer>
+    </div>
+  )
 }
 
 export async function getStaticProps(context) {
-    return {
-        props: {}, // will be passed to the page component as props
-    }
+  return {
+    props: {}, // will be passed to the page component as props
+  }
 }
